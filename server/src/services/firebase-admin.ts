86525b4edeee4a78c4ff4f4f0b3b7a96c8e1e108
akshatsha5ill <akshatsharma8172@gmail.com>
2026-08-03 11:51:05 +0530

    commit

--- server/src/services/firebase-admin.ts
+++ server/src/services/firebase-admin.ts
@@ -9,7 +9,11 @@ let app: App | undefined;
 if (!getApps().length) {
   const projectId = process.env.FIREBASE_PROJECT_ID;
   const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
-  const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
+  // Automatically fix common copy-paste errors: remove surrounding quotes and fix newlines
+  let privateKey = process.env.FIREBASE_PRIVATE_KEY;
+  if (privateKey) {
+    privateKey = privateKey.replace(/^["']|["']$/g, '').replace(/\\n/g, '\n');
+  }
 
   if (projectId && clientEmail && privateKey) {
     app = initializeApp({
